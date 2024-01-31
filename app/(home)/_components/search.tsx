2259cc498826a4623de724f54713756f53f73929
Guilherme Santos <110872956+GuiZeroUm@users.noreply.github.com>
2024-01-30 23:31:58 -0500

    feat: add details components and google autenticator

--- app/(home)/_components/search.tsx
+++ app/(home)/_components/search.tsx
@@ -5,14 +5,14 @@ import { Input } from "@/app/_components/ui/input";
 import { SearchIcon } from "lucide-react";
 
 const Search = () => {
-    return ( 
-        <div className="flex items-center gap-2">
-            <Input placeholder="Busque por uma barbearia..."/>
-            <Button variant="default">
-                <SearchIcon size={20}/>
-            </Button>
-        </div>
-     );
-}
- 
+  return (
+    <div className="flex items-center gap-2">
+      <Input placeholder="Busque por uma barbearia..." />
+      <Button variant="default">
+        <SearchIcon size={20} />
+      </Button>
+    </div>
+  );
+};
+
 export default Search;
--- prisma/migrations/20240131001147_add_user_tables/migration.sql
+++ prisma/migrations/20240131001147_add_user_tables/migration.sql
@@ -1,19 +1,9 @@
 /*
   Warnings:
 
-  - You are about to drop the column `imgUrl` on the `Barbershop` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `imageUrl` to the `Barbershop` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `imageUrl` to the `Service` table without a default value. This is not possible if the table is not empty.
 
 */
--- AlterTable
-ALTER TABLE "Barbershop" DROP COLUMN "imgUrl",
-ADD COLUMN     "imageUrl" TEXT NOT NULL;
-
--- AlterTable
-ALTER TABLE "Service" ADD COLUMN     "imageUrl" TEXT NOT NULL;
-
 -- AlterTable
 ALTER TABLE "User" ADD COLUMN     "email" TEXT,
 ADD COLUMN     "emailVerified" TIMESTAMP(3),
